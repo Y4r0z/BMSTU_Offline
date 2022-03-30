c506b487f34b2b87be910aad9cb1ee1eee6bc37a
Yaroslav Bogdanov <jarosl.s@yandex.ru>
2022-03-30 12:57:27 +0300

    Чистка

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import os
 from pathlib import Path
 import sys
 from PySide6.QtWidgets import *
-from MainWindow import MainWindow
 from AuthWindow import AuthWindow
 from UniversalWindow import UniversalWindow
 if __name__ == "__main__":
     app = QApplication([])
-    #widget = MainWindow()
-    #widget.hide()
     widget = UniversalWindow()
     widget.hide()
     auth = AuthWindow(widget)
