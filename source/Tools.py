7d3d5cd6a24ef06b21f9b6e5af039c7d0a8af566
Yaroslav Bogdanov <jarosl.s@yandex.ru>
2022-07-19 19:47:05 +0300

    Улучшен поиск
    Теперь используется библиотека fuzzywuzzy, которая обеспечивает неточное сравнение.

--- source/Tools.py
+++ source/Tools.py
@@ -1,9 +1,14 @@
- 
+from fuzzywuzzy import fuzz
+
 def stringCmp(original, toFind):
     if len(toFind) == 0:
         return True
-    if original.lower().find(toFind) != -1 or invertString(original).find(toFind) != -1:
+    if original.lower().find(toFind) != -1\
+         or original.lower().find(invertString(toFind)) != -1:
         return True
+    for word in original.split(' '):
+        if fuzz.token_set_ratio(word, toFind) > 60:
+            return True
     return False
 
 def stringFind(original, search):
@@ -11,12 +16,6 @@ def stringFind(original, search):
     for word in search.split(' '):
         if not stringCmp(original, word):
             flag = False
-    if not flag:
-        inverted = invertString(search)
-        flag = True
-        for word in inverted.split(' '):
-            if not stringCmp(original, word):
-                flag = False
     return flag
 
 def invertString(string):
@@ -28,3 +27,4 @@ def invertString(string):
         else:
             text.append(i)
     return ''.join(text)
+
